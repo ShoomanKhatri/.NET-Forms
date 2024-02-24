cfaa4a0b7f3684970d1443b515cafc4e6ed775b3
Shooman Khatri <112597601+ShoomanKhatri@users.noreply.github.com>
2024-02-24 23:18:45 +0545

    first

---
+++ WebApplication2/WebForm1.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+       <div>
+            <label for="name">Name:</label>
+            <asp:TextBox ID="txtName" runat="server"></asp:TextBox><br /><br />
+            
+            <label for="email">Email:</label>
+            <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox><br /><br />
+            
+            <label for="message">Message:</label>
+            <asp:TextBox ID="txtMessage" runat="server" TextMode="MultiLine" Rows="4" OnTextChanged="txtMessage_TextChanged"></asp:TextBox><br /><br />
+            
+            <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication2/WebForm1.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            // Get the connection string from web.config file
+            string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+
+            // Create connection
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                // Define the SQL query to insert data
+                string query = "INSERT INTO YourTableName (Name, Email, Message) VALUES (@Name, @Email, @Message)";
+
+                // Create command and parameters
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                    cmd.Parameters.AddWithValue("@Message", txtMessage.Text);
+
+                    // Open connection
+                    con.Open();
+
+                    // Execute the command
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            // Optionally, you can clear the form fields after submission
+            txtName.Text = "";
+            txtEmail.Text = "";
+            txtMessage.Text = "";
+
+            // You can also redirect the user to another page after submission if needed
+            // Response.Redirect("ThankYouPage.aspx");
+        }
+
+        protected void txtMessage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
